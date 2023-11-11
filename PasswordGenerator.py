fb8d310edb0df5ad94c847c0fa6449a0f47f8e63
Artin <artinnikjoo88@gmail.com>
2023-11-12 02:39:47 +0330

    New features + some changes

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -2,10 +2,11 @@ import tkinter as tk
 from random import choices
 from string import ascii_letters, digits
 from tkinter import font
+from tkinter import messagebox
 from PIL import ImageTk, Image
 
 if __name__ == "__main__":
-    print(">> welcome to password generator v1.0, developed by: Artin (Ketchup d) <<")
+    print(">> welcome to password generator v1.1, developed by: Artin (Ketchup d) <<")
 
 
 def randpass(length):
@@ -14,15 +15,11 @@ def randpass(length):
 
 
 def password(nothing=0):
-    global entry, textpassword, lengtherror, nopassworderror, theme, copy2clip
+    global entry, textpassword, lenerr_msg, theme, copy2clip
 
-    if nopassworderror != 0:
-        nopassworderror.destroy()
-        nopassworderror = 0
-
-    if lengtherror != 0:
-        lengtherror.destroy()
-        lengtherror = 0
+    if lenerr_msg != 0:
+        lenerr_msg.destroy()
+        lenerr_msg = 0
 
     if textpassword != 0:
         textpassword.destroy()
@@ -44,20 +41,21 @@ def password(nothing=0):
             pass
         elif length <= 0:
             if theme.cget("image") == "pyimage2":
-                lengtherror = tk.Label(frame_error,
-                                       text="password length cannot\n be zero or less.", font=font12, fg="#1A1A1A")
+                lenerr_msg = tk.Label(frame_msg,
+                                      text="password length cannot\n be zero or less.", font=font12, fg="#1A1A1A")
             elif theme.cget("image") == "pyimage3":
-                lengtherror = tk.Label(frame_error,
-                                       text="password length cannot\n be zero or less.", font=font12, fg="#f0f0f0", bg="#1A1A1A")
-            lengtherror.pack(pady=20)
+                lenerr_msg = tk.Label(frame_msg,
+                                      text="password length cannot\n be zero or less.", font=font12, fg="#f0f0f0", bg="#1A1A1A")
+            lenerr_msg.pack(pady=20)
+            window.after(3000, lenerr_msg.destroy)
         else:
             if theme.cget("image") == "pyimage2":
                 copy2clip = tk.Button(frame_copy2clip, text="copy to clipboard",
-                                      bg="#f0f0f0", command=lambda: ctcb(textpassword.get("1.0", tk.END)))
+                                      bg="#f0f0f0", relief="solid", borderwidth=1, command=lambda: c2c(textpassword.get("1.0", tk.END)))
                 copy2clip.pack(pady=15)
             if theme.cget("image") == "pyimage3":
                 copy2clip = tk.Button(frame_copy2clip, text="copy to clipboard",
-                                      bg="#1A1A1A", fg="white", command=lambda: ctcb(textpassword.get("1.0", tk.END)))
+                                      bg="#1A1A1A", fg="white", relief="solid", borderwidth=1, command=lambda: c2c(textpassword.get("1.0", tk.END)))
                 copy2clip.pack(pady=15)
             if theme.cget("image") == "pyimage2":
                 textpassword = tk.Text(frame_password, font=font14,
@@ -70,52 +68,35 @@ def password(nothing=0):
 
 
 def txt(nothing=0):
-    global lengtherror, nopassworderror, theme, copy2clip
-
-    if nopassworderror != 0:
-        nopassworderror.destroy()
-        nopassworderror = 0
-
-    if lengtherror != 0:
-        lengtherror.destroy()
-        lengtherror = 0
+    global theme, copy2clip
 
     try:
         if textpassword:
             password = textpassword.get("1.0", tk.END)
             with open("password.txt", "w") as txt:
                 txt.write(password)
+            messagebox.showinfo(
+                "Done", "Generated password has been saved in the application directory.")
         else:
             if copy2clip != 0:
                 copy2clip.destroy()
                 copy2clip = 0
-            if theme.cget("image") == "pyimage2":
-                nopassworderror = tk.Label(frame_error,
-                                           text="Please generate a password first", font=font12, fg="#1A1A1A")
-            elif theme.cget("image") == "pyimage3":
-                nopassworderror = tk.Label(frame_error,
-                                           text="Please generate a password first", font=font12, fg="#f0f0f0",  bg="#1A1A1A")
-            nopassworderror.pack(pady=20)
+            messagebox.showerror(
+                "No password", "Please generate a password first.")
     except Exception:
         if copy2clip != 0:
             copy2clip.destroy()
             copy2clip = 0
-        if theme.cget("image") == "pyimage2":
-            nopassworderror = tk.Label(frame_error,
-                                       text="Please generate a password first", font=font12, fg="#1A1A1A")
-        elif theme.cget("image") == "pyimage3":
-            nopassworderror = tk.Label(frame_error,
-                                       text="Please generate a password first", font=font12, fg="#f0f0f0",  bg="#1A1A1A")
-        nopassworderror.pack(pady=20)
+        messagebox.ERROR("No password", "Please generate a password first.")
 
 
 def themechanger():
-    global frame, frame_error, frame_password, frame_theme, lengtherror, textpassword, nopassworderror, entry, button, label, labelcopyright, theme, dark, light, window, frame_copy2clip, copy2clip
+    global frame, frame_msg, frame_password, frame_theme, lenerr_msg, textpassword, entry, button, label, labelcopyright, theme, dark, light, window, frame_copy2clip, copy2clip
 
     if theme.cget("image") == "pyimage2":
         window.config(bg="#1A1A1A")
         frame.config(bg="#1A1A1A")
-        frame_error.config(bg="#1A1A1A")
+        frame_msg.config(bg="#1A1A1A")
         frame_password.config(bg="#1A1A1A")
         frame_theme.config(bg="#1A1A1A")
         frame_copy2clip.config(bg="#1A1A1A")
@@ -124,21 +105,18 @@ def themechanger():
         button.config(image=buttonimg_dark, bg="#1A1A1A")
         labelcopyright.config(bg="#1A1A1A")
 
-        if lengtherror != 0:
-            lengtherror.config(fg="#f0f0f0", bg="#1A1A1A")
+        if lenerr_msg != 0:
+            lenerr_msg.config(fg="#f0f0f0", bg="#1A1A1A")
         if textpassword != 0:
             textpassword.config(fg="white", bg="#212121")
-        if nopassworderror != 0:
-            nopassworderror.config(fg="#f0f0f0", bg="#1A1A1A")
         if copy2clip != 0:
             copy2clip.config(bg="#212121", fg="white")
-
         theme.config(image=light, bg="#1A1A1A")
 
     elif theme.cget("image") == "pyimage3":
         window.config(bg="#f0f0f0")
         frame.config(bg="#f0f0f0")
-        frame_error.config(bg="#f0f0f0")
+        frame_msg.config(bg="#f0f0f0")
         frame_password.config(bg="#f0f0f0")
         frame_theme.config(bg="#f0f0f0")
         frame_copy2clip.config(bg="#f0f0f0")
@@ -147,26 +125,22 @@ def themechanger():
         button.config(image=buttonimg_light, bg="#f0f0f0")
         labelcopyright.config(bg="#f0f0f0")
 
-        if lengtherror != 0:
-            lengtherror.config(fg="#1A1A1A", bg="#f0f0f0")
+        if lenerr_msg != 0:
+            lenerr_msg.config(fg="#1A1A1A", bg="#f0f0f0")
         if textpassword != 0:
             textpassword.config(fg="black", bg="#f0f0f0")
-        if nopassworderror != 0:
-            nopassworderror.config(fg="#1A1A1A", bg="#f0f0f0")
         if copy2clip != 0:
             copy2clip.config(bg="#f0f0f0", fg="black")
-
         theme.config(image=dark, bg="#f0f0f0")
 
 
-def ctcb(text):
+def c2c(text):
     window.clipboard_clear()
     window.clipboard_append(text)
 
 
-lengtherror = 0
+lenerr_msg = 0
 textpassword = 0
-nopassworderror = 0
 copy2clip = 0
 
 window = tk.Tk()
@@ -205,7 +179,8 @@ light = ImageTk.PhotoImage(Image.open(
 
 frame_theme = tk.Frame(window, width=30, height=30)
 frame_theme.pack(side="top", fill="both", pady=5, padx=5)
-theme = tk.Button(frame_theme, image=dark, command=themechanger, borderwidth=0)
+theme = tk.Button(frame_theme, image=dark,
+                  command=themechanger, borderwidth=0)
 theme.pack(side="left")
 
 frame = tk.Frame(window)
@@ -217,8 +192,8 @@ frame_copy2clip.pack(side="bottom")
 frame_password = tk.Frame(window)
 frame_password.pack(pady=20)
 
-frame_error = tk.Frame(window)
-frame_error.pack(side="bottom")
+frame_msg = tk.Frame(window)
+frame_msg.pack(side="bottom")
 
 label = tk.Label(frame, text="Password length:",
                  fg="#373737", font=font18)
